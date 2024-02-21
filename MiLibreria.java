692fd096796ef8d9c9f1482fa6f7819adcb276cc
SebasT2005 <160771007+SebasT2005@users.noreply.github.com>
2024-02-21 12:34:32 -0500

    Create MiLibreria.java

---
+++ MiLibreria.java
@@ -0,0 +1,16 @@
+public class MiLibreria {
+
+    public void funcionUno() {
+        //función uno
+    }
+
+    public int funcionDos(int parametro) {
+        //función dos
+        return parametro * 2;
+    }
+
+    public String funcionTres(String texto) {
+        //función tres
+        return "¡Hola, " + texto + "!";
+    }
+}
